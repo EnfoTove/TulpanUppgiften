ec5b191c928ce823945d5062cd318f81c6be6624
EnfoToveSeger <tove.seger@enfogroup.com>
2022-09-09 10:23:53 +0200

    small changes

--- lib/webparts/tulipList/components/TulipList.module.scss.d.ts
+++ lib/webparts/tulipList/components/TulipList.module.scss.d.ts
@@ -1,9 +1,6 @@
 declare const styles: {
     tulipList: string;
     container: string;
-    row: string;
-    column: string;
-    'ms-Grid': string;
     title: string;
     subTitle: string;
     defaultButton: string;
